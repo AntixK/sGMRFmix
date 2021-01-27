c8a1824377064dbb9de65edd3de88c05b671e0ba
Anand <anandktish904@gmail.com>
2021-01-28 08:20:14 +0900

    Added dependencies for build

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -15,6 +15,17 @@ ln -s /opt/python/cp37-cp37m/bin/cmake /usr/bin/cmake
 
 
 # Install a system package required by our library
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    if [[ -f /etc/lsb-release ]];then
+        apt-get update -qq
+        apt-get install -qq libgsl0-dev libarmadillo-dev libboost-all-dev
+    elif [[ -f /etc/centos-release ]] || [[ -f /etc/redhat-release ]]; then
+        yum -y install blas-devel lapack-devel boost-devel armadillo-devel gsl-devel
+    fi
+
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install gsl armadillo boost
+fi
 
 # Compile wheels
 for PYBIN in /opt/python/cp3*/bin; do
