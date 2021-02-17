3220fb06a10d7827ea66c0ba5ba3787206e21623
Anand <anandktish904@gmail.com>
2021-02-17 13:24:35 +0900

    Updated travis build

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -10,27 +10,28 @@ function repair_wheel {
     fi
 }
 
-/opt/python/cp37-cp37m/bin/pip install twine cmake
-ln -s /opt/python/cp37-cp37m/bin/cmake /usr/bin/cmake
+# /opt/python/cp37-cp37m/bin/pip install twine cmake
+# ln -s /opt/python/cp37-cp37m/bin/cmake /usr/bin/cmake
 
 
 # Install a system package required by our library
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     if [[ -f /etc/lsb-release ]];then
         apt-get update -qq
-        apt-get install -qq libarmadillo-dev libboost-all-dev build-essential
+        apt-get install -qq libopenblas-dev liblapack-dev libarpack2-dev libsuperlu-dev 
+        apt-get install -qq libboost-all-dev build-essential
     elif [[ -f /etc/centos-release ]] || [[ -f /etc/redhat-release ]]; then
-        yum -y install blas-devel lapack-devel boost-devel armadillo-devel 
+        yum -y install blas-devel lapack-devel boost-devel  
     fi
 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    brew install armadillo boost
+    brew install boost
 fi
 
 # Compile wheels
 for PYBIN in /opt/python/cp3*/bin; do
     VER=$(echo $PYBIN| cut -b 16)   # Get the python version
-    if [ $VER -gt 6 ]               # Build only for python verion greater than 3.6
+    if [ $VER -gt 5 ]               # Build only for python verion greater than 3.6
     then
         "${PYBIN}/pip" install -r /io/requirements.txt
         "${PYBIN}/pip" wheel /io/ -w wheelhouse/
@@ -47,7 +48,7 @@ done
 # Install packages and test
 for PYBIN in /opt/python/cp3*/bin/; do
     VER=$(echo $PYBIN| cut -b 16)   # Get the python version
-    if [ $VER -gt 6 ]
+    if [ $VER -gt 5 ]
     then
         "${PYBIN}/pip" install -r /io/requirements.txt
         "${PYBIN}/pip" install --no-index -f /io/wheelhouse sgmrfmix
